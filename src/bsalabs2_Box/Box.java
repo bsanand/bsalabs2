420694048ed3ecb7702ad2814475916c9550b141
bsanand <bshashianand@yahoo.com>
2013-12-08 15:47:38 -0800

    Added a new constructor

--- src/bsalabs2_Box/Box.java
+++ src/bsalabs2_Box/Box.java
@@ -2,6 +2,15 @@ package bsalabs2_Box;
 
 public class Box {
 
+	private int m_length=0;
+	private int m_breadth=0;
+	private int m_height=0;
+	
+	public Box(int len, int breadth, int height){
+		m_length = len;
+		m_breadth = breadth;
+		m_height = height;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Box");
